d6f433b777586186904b53a3f36766cf15ea4251
SanDiegos <52674560+SanDiegos@users.noreply.github.com>
2019-07-09 01:02:37 +0200

    Add files via upload

--- WeatherApp v3/src/main/java/com/djedra/WeatherApp/api/WeatherGUI.java
+++ WeatherApp v3/src/main/java/com/djedra/WeatherApp/api/WeatherGUI.java
@@ -106,9 +106,9 @@ public class WeatherGUI extends VerticalLayout {
 		return Objects.nonNull(textAreaLog.getValue()) && textAreaLog.getValue().length() > 0
 				? textAreaLog.getValue() + "\n\n" + LocalDate.now() + " " + LocalTime.now()
 						+ "\nPobrano dane pogodowe z adresu: " + WeatherAPIAdress.META_WEATHER.getUrl() + "\n"
-						+ "Miasto" + w.getCity()
+						+ "Miasto: " + w.getCity()
 				: LocalDate.now() + " " + LocalTime.now() + "\nPobrano dane pogodowe z adresu: "
-						+ WeatherAPIAdress.META_WEATHER.getUrl() + "\n" + "Miasto" + w.getCity();
+						+ WeatherAPIAdress.META_WEATHER.getUrl() + "\n" + "Miasto: " + w.getCity();
 	}
 
 }
